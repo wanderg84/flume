2242f7775f557c0183fbe68344b83123115f4d63
정승길(SEUNGGIL JUENG)/Onestore <sg.jueng@onestorecorp.com>
2017-02-10 15:44:24 +0900

    checkStyle 지적사항 이행

--- flume-ng-sources/flume-taildir-source/src/test/java/org/apache/flume/source/taildir/TailFileTest.java
+++ flume-ng-sources/flume-taildir-source/src/test/java/org/apache/flume/source/taildir/TailFileTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.flume.source.taildir;
 
 import junit.framework.Assert;
@@ -11,34 +30,29 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 
-/**
- * Created by 9230095 on 2017. 2. 9..
- */
 public class TailFileTest {
 
-    private File tmpDir;
+  private File tmpDir;
 
-    @Test
-    public void testCreationTime() throws IOException {
+  @Test
+  public void testCreationTime() throws IOException {
+    //given
+    File f1 = new File(tmpDir, "file1");
+    if (f1.exists()) f1.delete();
 
-        //given
-        File f1 = new File(tmpDir, "file1");
-        if (f1.exists()) f1.delete();
+    f1.createNewFile();
+    long inode = (long) Files.getAttribute(f1.toPath(), "unix:ino");
 
-        f1.createNewFile();
-        long inode = (long) Files.getAttribute(f1.toPath(), "unix:ino");
+    //when
+    TailFile tf = new TailFile(f1, new HashMap<String, String>(), inode, 0);
 
-        //when
-        TailFile tf = new TailFile(f1, new HashMap<String, String>(), inode, 0);
+    //then
+    FileTime fileTime = tf.getCreationTime();
+    Assert.assertNotNull(fileTime);
 
-        //then
-        FileTime fileTime = tf.getCreationTime();
-        Assert.assertNotNull(fileTime);
+    SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+    Assert.assertEquals(df.format(new Date()), df.format(fileTime.toMillis()));
 
-        SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-        Assert.assertEquals(df.format(new Date()), df.format(fileTime.toMillis()));
-
-        f1.deleteOnExit();
-    }
-
+    f1.deleteOnExit();
+  }
 }
